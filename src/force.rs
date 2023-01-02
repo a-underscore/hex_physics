3cd150eaa37ba71b3cf2f4fbadfa8c3e4ee0fad4
Brendan Bortz <brendansbortz@protonmail.com>
2023-01-02 15:24:17 -0500

    changed dependency

--- src/force.rs
+++ src/force.rs
@@ -1,4 +1,4 @@
-use hex::cgmath::Vector2;
+use hex::{cgmath::Vector2, cid, ecs::component_manager::Component};
 
 pub struct Kilos(f32);
 pub struct Mps(f32);
@@ -6,4 +6,10 @@ pub struct Mps(f32);
 pub struct Force {
     pub weight: Kilos,
     pub velocity: Vector2<Mps>,
+}
+
+impl Component for Force {
+    fn id() -> usize {
+        cid!()
+    }
 }
