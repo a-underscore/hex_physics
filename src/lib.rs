61b01a0666557c4ff0c4c0e6d26034b55d65e42f
Brendan Bortz <brendansbortz@protonmail.com>
2023-02-09 09:31:35 -0500

    b

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,8 @@ pub mod collider;
 pub mod collision_manager;
 pub mod physical;
 pub mod physics_manager;
+
+pub use collider::Collider;
+pub use collision_manager::CollisionManager;
+pub use physical::Physical;
+pub use physics_manager::PhysicsManager;
