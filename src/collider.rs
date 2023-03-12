c1499aa1b6f3e00c14be238f2622df68bbefaf35
Brendan Bortz <brendansbortz@protonmail.com>
2023-03-12 10:57:52 -0400

    b

--- src/collider.rs
+++ src/collider.rs
@@ -89,13 +89,13 @@ impl Collider {
             .points
             .iter()
             .cloned()
-            .map(|p| Vec2::truncate(transform.matrix() * p.extend(1.0)))
+            .map(|p| Vec2::trunc(&(transform.matrix() * p.extend(1.0))))
             .collect::<Vec<_>>();
         let b_points = b
             .points
             .iter()
             .cloned()
-            .map(|p| Vec2::truncate(b_transform.matrix() * p.extend(1.0)))
+            .map(|p| Vec2::trunc(&(b_transform.matrix() * p.extend(1.0))))
             .collect::<Vec<_>>();
 
         let mut min = None;
