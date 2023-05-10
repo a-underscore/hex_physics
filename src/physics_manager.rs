0b0c158289900114316b64df722cd70a4227c694
Brendan Bortz <brendansbortz@protonmail.com>
2023-05-10 10:59:37 -0400

    pre clippy

--- src/physics_manager.rs
+++ src/physics_manager.rs
@@ -113,37 +113,42 @@ impl PhysicsManager {
                     .then(|| e.clone())
             })
             .collect();
-        let mut checked = Vec::new();
 
-        for ((ae, ac, at), (be, bc, bt), (ghost, (atr, btr))) in
-            entities
-                .iter()
-                .flat_map(|(ae, (ac, a_col), (at, a_transform), a_physical)| {
-                    tree.query(Box2d::new(a_transform.position(), a_col.boundary))
-                        .into_iter()
-                        .filter_map(|(_, a)| {
-                            let (be, (bc, b_col), (bt, b_transform), b_physical) = &*a;
-                            let res = {
-                                if !checked.contains(&(ae, *be)) && !checked.contains(&(be, *ae)) {
+        for ((ae, ac, at), (be, bc, bt), (ghost, (atr, btr))) in entities
+            .iter()
+            .fold(
+                (Vec::new(), Vec::new()),
+                |(mut checked, mut res), (ae, (ac, a_col), (at, a_transform), a_physical)| {
+                    res.extend(
+                        tree.query(Box2d::new(a_transform.position(), a_col.boundary))
+                            .into_iter()
+                            .filter_map(|(_, a)| {
+                                let (be, (bc, b_col), (bt, b_transform), b_physical) = &*a;
+                                let res = if !checked.contains(&(*ae, *be))
+                                    && !checked.contains(&(*be, *ae))
+                                {
                                     Some((
                                         (ae, ac, at),
                                         (*be, *bc, *bt),
                                         Self::detect(
-                                            (a_col, a_transform, a_physical),
+                                            (&a_col, &a_transform, &a_physical),
                                             (b_col, b_transform, b_physical),
                                         )?,
                                     ))
                                 } else {
                                     None
-                                }
-                            };
+                                };
 
-                            checked.push((ae, *be));
+                                checked.push((*ae, *be));
 
-                            res
-                        })
-                        .collect::<Vec<_>>()
-                })
+                                res
+                            }),
+                    );
+
+                    (checked, res)
+                },
+            )
+            .1
         {
             Self::resolve(ghost, *ae, bc, bt, btr, cm);
             Self::resolve(ghost, be, *ac, *at, atr, cm);
